92b287ad0f44de27498e7f04a6be5c9d2d647f45
DevLabo <monolab000@gmail.com>
2023-11-01 13:47:25 +0900

    sidebarの選択時の背景が途中で切れてしまう問題を修正

--- src/components/layout/sidebar/menu/MenuPresenter.tsx
+++ src/components/layout/sidebar/menu/MenuPresenter.tsx
@@ -13,7 +13,7 @@ export function MenuPresenter(props: PropsTypes) {
     <button
       type="button"
       onClick={handleClick}
-      className={`mt-4 flex items-center px-6 py-2 ${
+      className={`w-full mt-4 flex items-center px-6 py-2 ${
         active
           ? 'bg-gray-700/25 text-gray-100'
           : 'text-gray-400 hover:bg-gray-700/25 hover:text-gray-100'
